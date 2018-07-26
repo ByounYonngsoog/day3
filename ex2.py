9edd00e23d38f3b0439630ad85db15fce3dc76d8
ByounYonngsoog <victoria69@hanmail.net>
2018-07-26 16:11:59 +0900

    계산기로또번호추천기

---
+++ ex2.py
@@ -0,0 +1,11 @@
+# from calculator import add, sub, mul, div
+from calculator import *
+
+result = add(10,20)
+print(result)
+result = sub(10,20)
+print(result)
+result = mul(10,20)
+print(result)
+result = div(10,20)
+print(result)
